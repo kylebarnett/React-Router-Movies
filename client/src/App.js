e31282f5f934a1a00ecb9d234d82fa2e7a2fae37
Kyle Barnett <kbarnett23@gmail.com>
2019-08-07 19:07:46 -0600

    added stretch

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ const App = props => {
     <div>
       <SavedList list={savedList} />
       <Route exact path="/" render={(props) => <MovieList {...props} />} />
-      <Route path="/movies/:id" render={(props) => <Movie {...props} />} />
+      <Route path="/movies/:id" render={(props) => <Movie {...props} addToSavedList={addToSavedList} />} />
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -22,10 +22,10 @@ const Movie = (props) => {
   }, []);
 
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -50,7 +50,7 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div onClick={() => props.addToSavedList(movie)} className="save-button">Save</div>
     </div>
   );
 }
